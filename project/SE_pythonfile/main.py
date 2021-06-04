376f693ff740816f2de1bbf373358aa4673fb634
An-hyeonyoung <ahy1612@naver.com>
2021-06-04 21:32:18 +0900

    Add documentation in python file

--- project/SE_pythonfile/main.py
+++ project/SE_pythonfile/main.py
@@ -3,22 +3,26 @@ from bs4 import BeautifulSoup
 import csv
 
 # 서울특별시 - 내 손안에 서울
-# 페이지에서 검색 crawling 이용은 어려움
-# basic_url = 'https://mediahub.seoul.go.kr/search/searchDetailList.do'
-# 실제 기사 url
+# get the web url in list
 web = ['https://mediahub.seoul.go.kr/archives/1161228', 'https://mediahub.seoul.go.kr/archives/2001152', 'https://mediahub.seoul.go.kr/archives/2000380']
 
+# create file to save the result of crawling
 f = open('src.csv', 'w',  encoding='utf-8-sig', newline='')
+# write method
 wr = csv.writer(f)
 
 # call html
+# http request for first web[0]
 response = requests.post(web[0])
+# get response of request
 html = response.text
+# BeatifulSoup library for view
 soup = BeautifulSoup(html, 'html.parser')
 title = ""
 content = ""
 imgUrl = ""
 
+# get information with tag
 for tag in soup.select('#fontScaleTarget > div.news_detail_view > div > div.boxing > h3'):
     title = tag.get_text()
 for tag in soup.select('#fontScaleTarget > div.cont_aside > div.cont > div > div.news_detail_cont.oldNews > table > tbody > tr > td'):
@@ -26,15 +30,21 @@ for tag in soup.select('#fontScaleTarget > div.cont_aside > div.cont > div > div
 for tag in soup.select('#fontScaleTarget > div.cont_aside > div.cont > div > div.news_detail_cont.oldNews > p:nth-child(1) > img'):
     imgUrl = tag['src']
 
+# write information to csv file
 wr.writerow([title, content, imgUrl])
 
+# call html
+# http request for first web[1]
 response = requests.post(web[1])
+# get response of request
 html = response.text
+# BeatifulSoup library for view
 soup = BeautifulSoup(html, 'html.parser')
 title = ""
 content = ""
 imgUrl = ""
 
+# get information with tag
 for tag in soup.select('#fontScaleTarget > div.news_detail_view > div > div.boxing > h3'):
     title = tag.get_text()
 for tag in soup.select('#fontScaleTarget > div.cont_aside > div.cont > div > div.news_detail_cont > div:nth-child(2) > div > div'):
@@ -42,15 +52,21 @@ for tag in soup.select('#fontScaleTarget > div.cont_aside > div.cont > div > div
 for tag in soup.select('#fontScaleTarget > div.cont_aside > div.cont > div > div.news_detail_cont > div:nth-child(3) > div > div.img > img'):
     imgUrl = tag['src']
 
+# write information to csv file
 wr.writerow([title, content, imgUrl])
 
+# call html
+# http request for first web[2]
 response = requests.post(web[2])
+# get response of request
 html = response.text
+# BeatifulSoup library for view
 soup = BeautifulSoup(html, 'html.parser')
 title = ""
 content = ""
 imgUrl = ""
 
+# get information with tag
 for tag in soup.select('#fontScaleTarget > div.news_detail_view > div > div.boxing > h3'):
     title = tag.get_text()
 for tag in soup.select('#fontScaleTarget > div.cont_aside > div.cont > div > div.news_detail_cont > div:nth-child(1) > div > div'):
@@ -58,6 +74,10 @@ for tag in soup.select('#fontScaleTarget > div.cont_aside > div.cont > div > div
 for tag in soup.select('#fontScaleTarget > div.cont_aside > div.cont > div > div.news_detail_cont > div:nth-child(2) > div > div.img > img'):
     imgUrl = tag['src']
 
+# write information to csv file
 wr.writerow([title, content, imgUrl])
 
+# end of crawling
+
+# close csv file
 f.close()
